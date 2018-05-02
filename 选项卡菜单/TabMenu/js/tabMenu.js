f8ed2ab365c3f2702cb7e082e3cadf9f7caf002f
MrShan1 <568906155@qq.com>
2018-05-02 11:26:48 +0800

    sc 公司更新

---
+++ 选项卡菜单/TabMenu/js/tabMenu.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="jquery.min.js" />
+
+(function () {
+    $(".vis_tabButton").click(function () {
+        var group = $(this).data("group");
+        var tabMenu = $(this).parents(".vis_top").find(".vis_tabMenu[data-group='" + group + "']")[0];
+
+        $(tabMenu).css("display", "block");
+        $(tabMenu).siblings(".vis_tabMenu").css("display", "none");
+    });
+})();
