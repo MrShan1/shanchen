07aec3dfc53ac651bb3b574b57613e9742f918e2
MrShan1 <568906155@qq.com>
2018-05-28 19:59:55 +0800

    sc 公司更新

--- 环形树布局/CircleTreeLayout/js/CircleTreeLayout.js
+++ 环形树布局/CircleTreeLayout/js/CircleTreeLayout.js
@@ -8,11 +8,30 @@
 function RadialTreeLayout() {
     go.Layout.call(this);
 
-    this._rootKey = null;
+    // 预定义根顶点的主键集合
+    this.defaultRootKeys = new go.List();
+    // 预定义根顶点集合
+    this.defaultRootVertexs = new go.List();
+    // 根顶点集合
+    this.rootVertexs = new go.List();
+    // 子树的划分模式,默认依据方向划分
+    this.splitMode = RadialTreeLayout.SplitByDirection;
 };
-
-// 径向树布局继承基类布局的所有属性和方法
 go.Diagram.inherit(RadialTreeLayout, go.Layout);
+
+/**
+* 子树的划分模式之一
+*
+* @property {go.EnumValue} 划分模式_依据方向
+*/
+RadialTreeLayout.SplitByDirection = new go.EnumValue(RadialTreeLayout, "SplitByDirection", 10);
+
+/**
+* 子树的划分模式之一
+*
+* @property {go.EnumValue} 划分模式_依据根顶点
+*/
+RadialTreeLayout.SplitByRoot = new go.EnumValue(RadialTreeLayout, "SplitByRoot", 11);
 
 /**
 * 生成布局网络
@@ -30,30 +49,37 @@ RadialTreeLayout.prototype.createNetwork = function () {
 * @override
 */
 RadialTreeLayout.prototype.doLayout = function (coll) {
+    // 创建网络
     if (this.network === null) {
         this.network = this.makeNetwork(coll);
     }
 
-    if (this.rootKey === null) {
+
+
+    if (this.splitMode === RadialTreeLayout.SplitByRoot) {
 
     }
 };
 
-/**
-* 获取或设置节点数
-*
-* @param value {String} 根节点主键
-* @return {String} 根节点主键
-*/
-Object.defineProperty(RadialTreeLayout.prototype, "rootKey", {
-    get: function () { return this._rootKey; },
-    set: function (value) {
-        if (this._rootKey !== value) {
-            this._rootKey = value;
-            this.invalidateLayout();
-        }
+RadialTreeLayout.prototype.getTrees = function (network) {
+    var vertexs = new go.List().addAll(network.vertexs);
+
+    while (vertexs.count > 0) {
+        var first = vertexs.first();
+        var tree = new RadialTree();
+
     }
-});
+};
+
+RadialTreeLayout.prototype.buildTreeRelation = function (network) {
+    //var 
+
+    //while (vertexs.count > 0) {
+    //    var first = vertexs.first();
+    //    var tree = new RadialTree();
+
+    //}
+};
 
 //#endregion 径向树布局
 
@@ -69,8 +95,6 @@ Object.defineProperty(RadialTreeLayout.prototype, "rootKey", {
 function RadialTreeNetwork() {
     go.LayoutNetwork.call(this);
 };
-
-// 径向树网络继承基类布局网络的所有属性和方法
 go.Diagram.inherit(RadialTreeNetwork, go.LayoutNetwork);
 
 /**
@@ -106,10 +130,52 @@ RadialTreeNetwork.prototype.createVertex = function () {
 */
 function RadialTreeVertex() {
     go.LayoutVertex.call(this);
+
+    this.parent = null;
+    this.children = new go.List();
+};
+go.Diagram.inherit(RadialTreeVertex, go.LayoutVertex);
+
+/**
+* 获取相关顶点集合
+*
+* 相关顶点是指与当前顶点在同一关系网络中的其他顶点.
+*
+* @return {go.Set} 相关顶点集合
+*/
+RadialTreeVertex.prototype.getRelativeVertexs = function () {
+    var coll = new go.Set();
+    var iterator = this.vertexes.iterator; // 直接关联的顶点集合
+
+    while (iterator.next()) {
+        var vertex = iterator.value;
+
+        // 将该顶点添加至结果集合
+        coll.add(vertex);
+        // 将该的相关顶点集合添加至结果集中
+        coll.addAll(vertex.getRelativeVertexs());
+    }
+
+    return coll;
 };
 
-// 径向树顶点继承基类布局顶点的所有属性和方法
-go.Diagram.inherit(RadialTreeVertex, go.LayoutVertex);
+RadialTreeVertex.prototype.getParent = function () {
+    if (this.parent === null && this.sourceVertexes.count > 0) {
+        this.parent = this.sourceVertexes.first();
+    }
+};
+
+RadialTreeVertex.prototype.getChildren = function () {
+    var iterator = this.destinationVertexes.iterator;
+
+    while (iterator.next()) {
+        var vertex = iterator.value;
+        if (vertex.parent === null) {
+            this.children.add(vertex);
+        }
+    }
+
+};
 
 //#endregion 径向树顶点
 
@@ -125,12 +191,26 @@ go.Diagram.inherit(RadialTreeVertex, go.LayoutVertex);
 function RadialTreeEdge() {
     go.LayoutEdge.call(this);
 };
-
-// 径向树边线继承基类布局边线的所有属性和方法
 go.Diagram.inherit(RadialTreeEdge, go.LayoutEdge);
 
 //#endregion 径向树边线
 
+
+
+
+//#region 径向树
+
+/**
+* 径向树的构造函数
+*/
+function RadialTree() {
+    // 根顶点
+    this.root = null;
+    // 顶点集合
+    this.vertexs = new go.List();
+};
+
+//#endregion 径向树
 
 
 
