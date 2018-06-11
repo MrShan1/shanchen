a048ecc596b891b708cd82a1327843c0e7fdb8a9
MrShan1 <568906155@qq.com>
2018-06-11 18:33:29 +0800

    sc 公司更新

---
+++ 无向树布局/AdvancedTreeLayout/js/AdvancedTreeLayout.js
@@ -0,0 +1,152 @@
+﻿/// <reference path="go-debug-1.8.7.js" />
+
+//#region 高级树布局
+
+/**
+* 高级树布局的构造函数
+*/
+function AdvancedTreeLayout() {
+    go.TreeLayout.call(this);
+
+    // 预定义根顶点对应的节点集合
+    this.defaultRootNodes = new go.Set();
+    // 是否允许有无向树标识
+    this.allowUndirectedTree = false;
+};
+go.Diagram.inherit(AdvancedTreeLayout, go.TreeLayout);
+
+/**
+* 生成布局网络对象
+*
+* @return {AdvancedTreeNetwork} 高级树网络
+* @override
+*/
+AdvancedTreeLayout.prototype.createNetwork = function () {
+    return new AdvancedTreeNetwork();
+};
+
+/**
+* 制作布局网络
+*
+* @param {Diagram|Group|Iterable.} coll 目标视图对象集合
+* @return {AdvancedTreeNetwork} 高级树网络
+* @override
+*/
+AdvancedTreeLayout.prototype.makeNetwork = function (coll) {
+    var network = go.TreeLayout.prototype.makeNetwork.apply(this, arguments);
+
+    // 允许有无向树时,根据默认根顶点控制边线方向
+    if (this.allowUndirectedTree === true) {
+        var standbyVertexes = new go.List().addAll(network.vertexes); // 等待处理的顶点集合
+        var standbyEdges = new go.List().addAll(network.edges); // 等待处理的边线集合
+        var iterator = this.defaultRootNodes.iterator; // 默认根顶点集合
+
+        // 遍历根顶点,重新设定边线的方向
+        while (iterator.next()) {
+            var node = iterator.value;
+            var vertex = network.findVertex(node);
+
+            if (vertex && standbyVertexes.contains(vertex)) {
+                // 控制顶点的所有边线方向
+                this.controlEdgeGoging(vertex, network, standbyVertexes, standbyEdges);
+            }
+        }
+    }
+
+    return network;
+};
+
+/**
+* 控制目标顶点的所有边线方向
+*
+* 使目标顶点的所有边线都是从目标顶点触发
+*
+* @param {AdvancedTreeVertex} vertex 目标顶点
+* @param {AdvancedTreeNetwork} network 目标顶点
+* @param {go.List} standbyVertexes 等待处理的顶点集合
+* @param {go.List} standbyEdges 等待处理的边线集合
+*/
+AdvancedTreeLayout.prototype.controlEdgeGoging = function (vertex, network, standbyVertexes, standbyEdges) {
+    var iterator = vertex.edges.iterator;
+
+    // 目标顶点视为已处理,从等待顶点集合中移除该顶点
+    standbyVertexes.remove(vertex);
+
+    // 遍历目标顶点的边线,修改边线的方向
+    while (iterator.next()) {
+        var edge = iterator.value;
+
+        if (standbyEdges.contains(edge)) {
+            if (edge.fromVertex !== vertex) {
+                // 逆转边线方向
+                network.reverseEdge(edge);
+            }
+
+            // 从等待边线集合中移除该边线
+            standbyEdges.remove(edge);
+
+            if (standbyVertexes.contains(edge.toVertex)) {
+                // 递归循环，控制到达顶点的所有边线方向
+                this.controlEdgeGoging(edge.toVertex, network, standbyVertexes, standbyEdges);
+            }
+        }
+    }
+};
+
+//#endregion 高级树布局
+
+//#region 高级树网络
+
+/**
+* 高级树网络的构造函数
+*/
+function AdvancedTreeNetwork() {
+    go.TreeNetwork.call(this);
+};
+go.Diagram.inherit(AdvancedTreeNetwork, go.TreeNetwork);
+
+/**
+* 生成网络的边线
+*
+* @return {AdvancedTreeEdge} 高级树边线
+* @override
+*/
+AdvancedTreeNetwork.prototype.createEdge = function () {
+    return new AdvancedTreeEdge();
+};
+
+/**
+* 生成网络的顶点
+*
+* @return {AdvancedTreeVertex} 高级树顶点
+* @override
+*/
+AdvancedTreeNetwork.prototype.createVertex = function () {
+    return new AdvancedTreeVertex();
+};
+
+//#endregion 高级树网络
+
+//#region 高级树顶点
+
+/**
+* 高级树顶点的构造函数
+*/
+function AdvancedTreeVertex() {
+    go.TreeVertex.call(this);
+};
+go.Diagram.inherit(AdvancedTreeVertex, go.TreeVertex);
+
+//#endregion 高级树顶点
+
+//#region 高级树边线
+
+/**
+* 高级树边线的构造函数
+*/
+function AdvancedTreeEdge() {
+    go.TreeEdge.call(this);
+};
+go.Diagram.inherit(AdvancedTreeEdge, go.TreeEdge);
+
+//#endregion 高级树边线
---
+++ 无向树布局/AdvancedTreeLayout/js/testDiagram.js
@@ -0,0 +1,172 @@
+﻿/// <reference path="go-debug-1.8.7.js" />
+
+var $ = go.GraphObject.make;
+var diagram = null;
+
+function init() {
+    createDiagram();
+
+    createLayout();
+
+    loadData();
+};
+
+function createDiagram() {
+    diagram =
+        $(go.Diagram, "diagramDiv",
+            {
+                initialAutoScale: go.Diagram.Uniform,
+                contentAlignment: go.Spot.Center,
+                "undoManager.isEnabled": false,
+                "animationManager.isEnabled": true,
+                //isTreePathToChildren: false,
+            }
+        );
+
+    diagram.layout = createLayout();
+
+    diagram.model = new go.GraphLinksModel();
+
+    diagram.nodeTemplate =
+        $(go.Node, "Auto",
+            {
+                //isLayoutPositioned: false,
+                width: 50,
+                height: 50,
+                locationSpot: go.Spot.Center,
+            },
+            //new go.Binding("position", "position", go.Point.parse).makeTwoWay(go.Point.stringify),
+            //new go.Binding("position", "bounds", function (b) {
+            //    return b.position;
+            //}).makeTwoWay(function (p, d) {
+            //    return new go.Rect(p.x, p.y, d.bounds.width, d.bounds.height);
+            //}),
+            new go.Binding("width", "importance", function (data, obj) {
+                return obj.width * data;
+            }),
+            new go.Binding("height", "importance", function (data, obj) {
+                return obj.height * data;
+            }),
+            $(go.Shape, "Circle",
+                new go.Binding("fill", "color")
+            ),
+            $(go.TextBlock,
+                {
+                    margin: 2
+                },
+                new go.Binding("text", "key"),
+                new go.Binding("scale", "importance", function (data, obj) {
+                    return 1 * data;
+                })
+            )
+        );
+
+    diagram.linkTemplate =
+        $(go.Link,
+            {
+                //isLayoutPositioned: false
+
+                //routing: go.Link.Orthogonal,
+
+                routing: go.Link.Normal,
+                //curve: go.Link.Bezier,
+            },
+            $(go.Shape),
+            $(go.Shape,
+                  {
+                      toArrow: "Standard",
+                      //stroke: "black",
+                      fill: "black",
+                      //strokeWidth: 2
+                  }
+            )
+        );
+};
+
+function createLayout() {
+    var layout =
+        $(AdvancedTreeLayout,
+            {
+                //isInitial: true,
+                //isOngoing: false,
+                isRouting: false,
+                setsChildPortSpot: false,
+                setsPortSpot: false,
+                allowUndirectedTree: true,
+            }
+        );
+
+    return layout;
+};
+
+function loadData() {
+    var model = diagram.model;
+
+    generateNodes(model, 20, 50);
+    generateLinks(model, 1, 5);
+
+    diagram.nodes.each(function (node) {
+        var count = node.findTreeParts().count;
+        //var count = node.findLinksOutOf().count;
+        var importance = 1;
+
+        if (count < 100) {
+            importance = 1 + Math.floor(count * 100 / 5) / 100 * 0.2;
+        }
+        else {
+            importance = 1 + Math.floor(count * 100 / 5) / 100 * 0.1;
+        }
+
+        diagram.model.setDataProperty(node.data, "importance", importance);
+    });
+
+    diagram.layoutDiagram(true);
+};
+
+function generateNodes(model, min, max) {
+    if (isNaN(min) || min < 0) min = 2;
+    if (isNaN(max) || max < min) max = min;
+    var nodeArray = [];
+    var numNodes = Math.floor(Math.random() * (max - min + 1)) + min;
+    for (var i = 0; i < numNodes; i++) {
+        var d = {
+            key: i.toString(),
+            color: go.Brush.randomColor()  // the node's color
+        };
+        //!!!???@@@ this needs to be customized to account for your chosen Node template
+        d.bounds = new go.Rect(0, 0, 70, 20);
+        nodeArray.push(d);
+    }
+
+    model.nodeDataArray = nodeArray;
+    //model.addNodeDataCollection(nodeArray);
+};
+
+function generateLinks(model, min, max) {
+    if (model.nodeDataArray.length < 2) return;
+    if (isNaN(min) || min < 1) min = 1;
+    if (isNaN(max) || max < min) max = min;
+    var linkArray = [];
+    // make two Lists of nodes to keep track of where links already exist
+    var nodes = new go.List();
+    nodes.addAll(model.nodeDataArray);
+    var available = new go.List();
+    available.addAll(nodes);
+    for (var i = 0; i < nodes.length; i++) {
+        var next = nodes.elt(i);
+        available.remove(next)
+        var children = Math.floor(Math.random() * (max - min + 1)) + min;
+        for (var j = 1; j <= children; j++) {
+            if (available.length === 0) break;
+            var to = available.elt(0);
+            available.remove(to);
+            linkArray.push({
+                from: next.key,
+                to: to.key
+            });
+        }
+    }
+
+    model.linkDataArray = linkArray;
+    //model.addLinkDataCollection(linkArray);
+};
