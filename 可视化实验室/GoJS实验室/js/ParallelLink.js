9542a39bcc68b57bfd347039c93c967a3412fc92
MrShan1 <568906155@qq.com>
2018-07-25 19:08:20 +0800

    sc 公司更新

--- 可视化实验室/GoJS实验室/js/ParallelLink.js
+++ 可视化实验室/GoJS实验室/js/ParallelLink.js
@@ -3,12 +3,33 @@
 //#region 平行链接
 
 /**
-* 链接的构造函数
+* 平行链接的构造函数
 */
 function ParallelLink() {
     go.Link.call(this);
+
+    // 链接起始端的曲线控制点1
+    this.fromDivergenceControlPoint1 = null;
+    // 链接起始端的曲线控制点2
+    this.fromDivergenceControlPoint2 = null;
+    // 链接到达端的曲线控制点1
+    this.toDivergenceControlPoint1 = null;
+    // 链接到达端的曲线控制点2
+    this.toDivergenceControlPoint2 = null;
 }
 go.Diagram.inherit(ParallelLink, go.Link);
+
+/**
+* @property {Number} 平行线两端的从分叉到平行的宽度
+*/
+ParallelLink.prototype.divergenceLength = 15;
+
+/**
+* @property {Boolean} 分叉为曲线表示
+*
+* true表示曲线,false表示直线
+*/
+ParallelLink.prototype.isDivergenceBezier = true;
 
 /**
 * @property {Number} 平行线两端的合并为一条线的宽度
@@ -16,215 +37,60 @@ go.Diagram.inherit(ParallelLink, go.Link);
 ParallelLink.prototype.mergedLength = 5;
 
 /**
-* @property {Number} 平行线两端的从分叉到平行的宽度
+* @property {Number} 自反链接的间距
 */
-ParallelLink.prototype.divergentLength = 25;
+ParallelLink.prototype.reflexiveLinkSpace = 20;
 
 /**
-* 计算链接路由上的点
+* 复制链接
 *
-* @return {Boolean} 是否计算了路由上的点
+* @param {ParallelLink} copy 要进行复制的链接
 * @override
 */
-ParallelLink.prototype.computePoints = function () {
-    var result = go.Link.prototype.computePoints.call(this);
+ParallelLink.prototype.cloneProtected = function (copy) {
+    go.Link.prototype.cloneProtected.apply(this, arguments);
 
-    if (result === true && !this.isOrthogonal && this.curve !== go.Link.Bezier) {
-        var fromIndex = 0;
-        var toIndex = this.pointsCount - 1;
-
-        if (this.fromNode === this.toNode && this.pointsCount === 4) {
-            // 获取起始拐角点的x轴坐标
-            var fromX = this.getPoint(fromIndex + 1).x;
-            // 获取起始拐角点的y轴坐标
-            var fromY = this.getPoint(fromIndex).y;
-
-            // 获取到达拐角点的x轴坐标
-            var toX = this.getPoint(toIndex - 1).x;
-            // 获取到达拐角点的y轴坐标
-            var toY = this.getPoint(toIndex).y;
-
-            // 添加起始拐角点
-            this.insertPointAt(fromIndex + 1, fromX, fromY);
-            // 添加到达拐角点
-            this.insertPointAt(toIndex + 1, toX, toY);
-        }
-        else {
-            var fromPoint = this.getPoint(fromIndex); // 获取起始端的点
-            var toPoint = this.getPoint(toIndex); // 获取到达端的点
-            var centerPoint = this.midPoint; // 获取路由中间点
-            var dx = toPoint.x - fromPoint.x; // 获取两个端点在x轴上的距离
-            var dy = toPoint.y - fromPoint.y; // 获取两个端点在y轴上的距离
-            var ox = centerPoint.x - (fromPoint.x + dx * 0.5); // 获取中心点相对于端点路由的x轴偏移量
-            var oy = centerPoint.y - (fromPoint.y + dy * 0.5); // 获取中心点相对于端点路由的y轴偏移量
-            var mergedLength = this.mergedLength; // 获取合并长度
-            var divergentLength = this.divergentLength; // 获取分叉长度
-            var startPercent = 0; // 曲线起始点对应的线长比例
-            var controlPercent = 0; // 曲线控制点对应的线长比例
-            var endPercent = 0; // 曲线结束点对应的线长比例
-            var distance = Math.sqrt((dx * dx) + (dy * dy));
-
-            // 获取线长比例
-            if (dx !== 0 || dy !== 0) {
-                startPercent = mergedLength / distance;
-                controlPercent = (mergedLength + divergentLength / 2) / distance;
-                endPercent = (mergedLength + divergentLength) / distance;
-            }
-
-            // 获取起始端的曲线起始点的x轴坐标
-            var fromX1 = fromPoint.x + dx * startPercent;
-            // 获取起始端的曲线起始点的y轴坐标
-            var fromY1 = fromPoint.y + dy * startPercent;
-            // 获取起始端的曲线控制点1的x轴坐标
-            var fromX2 = fromPoint.x + dx * controlPercent;
-            // 获取起始端的曲线控制点1的y轴坐标
-            var fromY2 = fromPoint.y + dy * controlPercent;
-            // 获取起始端的曲线控制点2的x轴坐标
-            var fromX3 = fromPoint.x + dx * controlPercent + ox;
-            // 获取起始端的曲线控制点2的y轴坐标
-            var fromY3 = fromPoint.y + dy * controlPercent + oy;
-            // 获取起始端的曲线结束点的x轴坐标
-            var fromX4 = fromPoint.x + dx * endPercent + ox;
-            // 获取起始端的曲线结束点的y轴坐标
-            var fromY4 = fromPoint.y + dy * endPercent + oy;
-
-            // 获取到达端的曲线起始点的x轴坐标
-            var toX1 = fromPoint.x + dx * (1 - endPercent) + ox;
-            // 获取到达端的曲线起始点的y轴坐标
-            var toY1 = fromPoint.y + dy * (1 - endPercent) + oy;
-            // 获取到达端的曲线控制点1的x轴坐标
-            var toX2 = fromPoint.x + dx * (1 - controlPercent) + ox;
-            // 获取到达端的曲线控制点1的y轴坐标
-            var toY2 = fromPoint.y + dy * (1 - controlPercent) + oy;
-            // 获取到达端的曲线控制点2的x轴坐标
-            var toX3 = fromPoint.x + dx * (1 - controlPercent);
-            // 获取到达端的曲线控制点2的y轴坐标
-            var toY3 = fromPoint.y + dy * (1 - controlPercent);
-            // 获取到达端的曲线结束点的x轴坐标
-            var toX4 = fromPoint.x + dx * (1 - startPercent);
-            // 获取到达端的曲线结束点的y轴坐标
-            var toY4 = fromPoint.y + dy * (1 - startPercent);
-
-            // 添加起始端的的曲线起始点
-            this.insertPointAt(fromIndex + 1, fromX1, fromY1);
-            // 添加起始端的的曲线控制点1
-            this.insertPointAt(fromIndex + 2, fromX2, fromY2);
-            // 添加起始端的的曲线控制点2
-            this.insertPointAt(fromIndex + 3, fromX3, fromY3);
-            // 添加起始端的的曲线到达点
-            this.insertPointAt(fromIndex + 4, fromX4, fromY4);
-
-            // 添加到达端的的曲线起始点
-            this.insertPointAt(toIndex + 4, toX1, toY1);
-            // 添加到达端的的曲线控制点1
-            this.insertPointAt(toIndex + 5, toX2, toY2);
-            // 添加到达端的的曲线控制点2
-            this.insertPointAt(toIndex + 6, toX3, toY3);
-            // 添加到达端的的曲线到达点
-            this.insertPointAt(toIndex + 7, toX4, toY4);
-        }
-    }
-
-    return result;
+    copy.divergenceLength = this.divergenceLength;
+    copy.isDivergenceBezier = this.isDivergenceBezier;
+    copy.mergedLength = this.mergedLength;
+    copy.reflexiveLinkSpace = this.reflexiveLinkSpace;
 };
 
-//ParallelLink.prototype.getLinkPoint = function (node, port, spot, from, ortho, othernode, otherport) {
-//    if (this.fromNode === this.toNode) {
-//        var point = new go.Point(NaN, NaN);
-
-//        if (from === true) {
-//            var startPoint = this.fromNode.port.getDocumentPoint(go.Spot.Left);
-
-//            point.set(startPoint);
-//        }
-//        else {
-//            var endPoint = this.toNode.port.getDocumentPoint(go.Spot.Right);
-
-//            point.set(endPoint);
-//        }
-
-//        return point;
-//    }
-//    else {
-//        return go.Link.prototype.getLinkPoint.apply(this, arguments);
-//    }
-//};
-
+/**
+* 计算设置链接路由点的方式
+*
+* @return {go.EnumValue} 链接线类型
+* @override
+*/
 ParallelLink.prototype.computeCurve = function () {
+    // 自反链接时,强行改为折线模式
     if (this.fromNode === this.toNode) {
-        return go.Link.Normal;
+        return go.Link.None;
     }
     else {
         return go.Link.prototype.computeCurve.apply(this, arguments);
     }
 };
 
-ParallelLink.prototype.makeGeometry = function () {
-    if (this.fromNode === this.toNode) {
-        return go.Link.prototype.makeGeometry.apply(this, arguments);
-    }
-    else {
-        if (this.pointsCount === 11) {
-            var fromP0 = this.getPoint(0);
-            var fromP1 = this.getPoint(1);
-            var fromP2 = this.getPoint(2);
-            var fromP3 = this.getPoint(3);
-            var fromP4 = this.getPoint(4);
-            var midP = this.getPoint(5);
-            var toP0 = this.getPoint(6);
-            var toP1 = this.getPoint(7);
-            var toP2 = this.getPoint(8);
-            var toP3 = this.getPoint(9);
-            var toP4 = this.getPoint(10);
-
-            var path = new go.PathFigure(fromP0.x, fromP0.y, false);
-            path.add(new go.PathSegment(go.PathSegment.Line, fromP1.x, fromP1.y));
-            path.add(new go.PathSegment(go.PathSegment.Bezier, fromP4.x, fromP4.y, fromP2.x, fromP2.y, fromP3.x, fromP3.y));
-            path.add(new go.PathSegment(go.PathSegment.Line, midP.x, midP.y));
-            path.add(new go.PathSegment(go.PathSegment.Line, toP0.x, toP0.y));
-            path.add(new go.PathSegment(go.PathSegment.Bezier, toP3.x, toP3.y, toP1.x, toP1.y, toP2.x, toP2.y));
-            path.add(new go.PathSegment(go.PathSegment.Line, toP4.x, toP4.y));
-
-            var geo = new go.Geometry().add(path);
-            geo.normalize();
-        }
-        else {
-            var path = null;
-            for (var i = 0, length = this.pointsCount; i < length; i++) {
-                var p = this.getPoint(i);
-
-                if (path === null) {
-                    path = new go.PathFigure(p.x, p.y, false);
-                }
-                else {
-                    path.add(new go.PathSegment(go.PathSegment.Line, p.x, p.y));
-                }
-            }
-
-            var geo = new go.Geometry().add(path);
-            geo.normalize();
-        }
-
-
-        return geo;
-    }
-};
-
-ParallelLink.prototype.computeSpot = function (from, port) {
-    if (this.fromNode === this.toNode) {
-        return from === true ? go.Spot.Right : go.Spot.Left;
-    }
-    else {
-        return go.Link.prototype.computeSpot.apply(this, arguments);
-    }
-};
-
+/**
+* 计算链接的偏移量
+*
+* @param {Boolean} from 起始端标识
+* @param {go.GraphObject} port 作为端口的对象
+* @return {go.Spot} 端点
+* @override
+*/
 ParallelLink.prototype.computeCurviness = function (from, port) {
+    // 自反链接时,需要手动计算偏移量
     if (this.fromNode === this.toNode) {
         var links = this.fromNode.findLinksBetween(this.toNode);
+        var height = this.fromNode.actualBounds.height;
+        var space = this.reflexiveLinkSpace;
         var index = 0;
         var iterator = links.iterator;
+        var curviness = 0;
 
+        // 多个自反链接时,需要获取当前自反链接的排位
         while (iterator.next()) {
             var link = iterator.value;
 
@@ -236,12 +102,211 @@ ParallelLink.prototype.computeCurviness = function (from, port) {
             }
         }
 
-        var height = this.fromNode.actualBounds.height;
+        // 计算偏移量
+        curviness = height / 2 + 15 + index * space;
 
-        return height / 2 + 10 + index * 15;
+        return curviness;
     }
     else {
         return go.Link.prototype.computeCurviness.apply(this, arguments);
+    }
+};
+
+/**
+* 计算链接路由上的点
+*
+* @return {Boolean} 是否完成路由点的计算
+* @override
+*/
+ParallelLink.prototype.computePoints = function () {
+    var result = go.Link.prototype.computePoints.call(this);
+
+    // 只有go.Link.Normal时,才绘制平行线
+    if (result === true && !this.isOrthogonal && this.curve !== go.Link.Bezier) {
+        var fromIndex = 0;
+        var toIndex = this.pointsCount - 1;
+
+        // 自反链接时,单独处理
+        if (this.fromNode === this.toNode) {
+            // 获取链接起始端拐角点的x轴坐标
+            var fromX = this.getPoint(fromIndex + 1).x;
+            // 获取链接起始端拐角点的y轴坐标
+            var fromY = this.getPoint(fromIndex).y;
+
+            // 获取链接到达端拐角点的x轴坐标
+            var toX = this.getPoint(toIndex - 1).x;
+            // 获取链接到达端拐角点的y轴坐标
+            var toY = this.getPoint(toIndex).y;
+
+            // 添加链接路由点
+            this.insertPointAt(fromIndex + 1, fromX, fromY);
+            this.insertPointAt(toIndex + 1, toX, toY);
+        }
+        else {
+            var fromP = this.getPoint(fromIndex); // 获取起始端的点
+            var toP = this.getPoint(toIndex); // 获取到达端的点
+            var centerP = this.midPoint; // 获取路由中间点
+            var dx = toP.x - fromP.x; // 获取两个端点在x轴上的距离
+            var dy = toP.y - fromP.y; // 获取两个端点在y轴上的距离
+            var ox = centerP.x - (fromP.x + dx * 0.5); // 获取中心点相对于端点路由的x轴偏移量
+            var oy = centerP.y - (fromP.y + dy * 0.5); // 获取中心点相对于端点路由的y轴偏移量
+            var mergedLength = this.mergedLength; // 获取合并长度
+            var divergenceLength = this.divergenceLength; // 获取分叉长度
+            var startPercent = 0; // 曲线起始点对应的线长比例
+            var controlPercent = 0; // 曲线控制点对应的线长比例
+            var endPercent = 0; // 曲线结束点对应的线长比例
+            var distance = Math.sqrt((dx * dx) + (dy * dy)); // 起始点与结束点的间距长度
+
+            // 获取线长比例
+            if (dx !== 0 || dy !== 0) {
+                startPercent = mergedLength / distance;
+                controlPercent = (mergedLength + divergenceLength / 2) / distance;
+                endPercent = (mergedLength + divergenceLength) / distance;
+            }
+
+            // 获取链接起始端的曲线起始点的x轴坐标
+            var fromSPX = fromP.x + dx * startPercent;
+            // 获取链接起始端的曲线起始点的y轴坐标
+            var fromSPY = fromP.y + dy * startPercent;
+            // 获取链接起始端的曲线结束点的x轴坐标
+            var fromEPX = fromP.x + dx * endPercent + ox;
+            // 获取链接起始端的曲线结束点的y轴坐标
+            var fromEPY = fromP.y + dy * endPercent + oy;
+
+            // 获取链接到达端的曲线起始点的x轴坐标
+            var toSPX = fromP.x + dx * (1 - endPercent) + ox;
+            // 获取链接到达端的曲线起始点的y轴坐标
+            var toSPY = fromP.y + dy * (1 - endPercent) + oy;
+            // 获取链接到达端的曲线结束点的x轴坐标
+            var toEPX = fromP.x + dx * (1 - startPercent);
+            // 获取链接到达端的曲线结束点的y轴坐标
+            var toEPY = fromP.y + dy * (1 - startPercent);
+
+            // 添加链接路由点
+            this.insertPointAt(fromIndex + 1, fromSPX, fromSPY);
+            this.insertPointAt(fromIndex + 2, fromEPX, fromEPY);
+            this.insertPointAt(toIndex + 2, toSPX, toSPY);
+            this.insertPointAt(toIndex + 3, toEPX, toEPY);
+
+            // 分叉为曲线时，需要设置曲线控制点
+            if (this.isDivergenceBezier === true) {
+                // 获取起始端的曲线控制点1的x轴坐标
+                var fromCP1X = fromP.x + dx * controlPercent;
+                // 获取起始端的曲线控制点1的y轴坐标
+                var fromCP1Y = fromP.y + dy * controlPercent;
+                // 获取起始端的曲线控制点2的x轴坐标
+                var fromCP2X = fromP.x + dx * controlPercent + ox;
+                // 获取起始端的曲线控制点2的y轴坐标
+                var fromCP2Y = fromP.y + dy * controlPercent + oy;
+
+                // 获取到达端的曲线控制点1的x轴坐标
+                var toCP1X = fromP.x + dx * (1 - controlPercent) + ox;
+                // 获取到达端的曲线控制点1的y轴坐标
+                var toCP1Y = fromP.y + dy * (1 - controlPercent) + oy;
+                // 获取到达端的曲线控制点2的x轴坐标
+                var toCP2X = fromP.x + dx * (1 - controlPercent);
+                // 获取到达端的曲线控制点2的y轴坐标
+                var toCP2Y = fromP.y + dy * (1 - controlPercent);
+
+                // 设置控制点
+                this.fromDivergenceControlPoint1 = new go.Point(fromCP1X, fromCP1Y);
+                this.fromDivergenceControlPoint2 = new go.Point(fromCP2X, fromCP2Y);
+                this.toDivergenceControlPoint1 = new go.Point(toCP1X, toCP1Y);
+                this.toDivergenceControlPoint2 = new go.Point(toCP2X, toCP2Y);
+            }
+        }
+    }
+
+    return result;
+};
+
+/**
+* 计算链接两端的端点
+*
+* @param {Boolean} from 起始端标识
+* @param {go.GraphObject} port 作为端口的对象
+* @return {go.Spot} 端点
+* @override
+*/
+ParallelLink.prototype.computeSpot = function (from, port) {
+    if (this.fromNode === this.toNode) {
+        return from === true ? go.Spot.Right : go.Spot.Left;
+    }
+    else {
+        return go.Link.prototype.computeSpot.apply(this, arguments);
+    }
+};
+
+/**
+* 绘制链接图形
+*
+* @return {go.Geometry} 链接图形
+* @override
+*/
+ParallelLink.prototype.makeGeometry = function () {
+    if (this.fromNode === this.toNode) {
+        return go.Link.prototype.makeGeometry.apply(this, arguments);
+    }
+    else {
+        var fromIndex = 0;
+        var toIndex = this.pointsCount - 1;
+        var geo = new go.Geometry();
+
+        if (this.pointsCount === 7 && this.isDivergenceBezier === true) {
+            var fromCP1 = this.fromDivergenceControlPoint1; // 获取链接起始端的曲线控制点1
+            var fromCP2 = this.fromDivergenceControlPoint2; // 获取链接起始端的曲线控制点2
+            var toCP1 = this.toDivergenceControlPoint1;// 获取链接到达端的曲线控制点1
+            var toCP2 = this.toDivergenceControlPoint2;// 获取链接到达端的曲线控制点2
+
+            // 获取路由上的点
+            var fromP = this.getPoint(fromIndex);
+            var fromSP = this.getPoint(fromIndex + 1);
+            var fromEP = this.getPoint(fromIndex + 2);
+            var midP = this.getPoint(fromIndex + 3);
+            var toSP = this.getPoint(toIndex - 2);
+            var toEP = this.getPoint(toIndex - 1);
+            var toP = this.getPoint(toIndex);
+
+            // 创建链接线(平滑曲线)
+            var path = new go.PathFigure(fromP.x, fromP.y, false);
+            path.add(new go.PathSegment(go.PathSegment.Line, fromSP.x, fromSP.y));
+            path.add(new go.PathSegment(go.PathSegment.Bezier, fromEP.x, fromEP.y, fromCP1.x, fromCP1.y, fromCP2.x, fromCP2.y));
+            path.add(new go.PathSegment(go.PathSegment.Line, midP.x, midP.y));
+            path.add(new go.PathSegment(go.PathSegment.Line, toSP.x, toSP.y));
+            path.add(new go.PathSegment(go.PathSegment.Bezier, toEP.x, toEP.y, toCP1.x, toCP1.y, toCP2.x, toCP2.y));
+            path.add(new go.PathSegment(go.PathSegment.Line, toP.x, toP.y));
+
+            // 绘制链接的几何图形
+            geo.add(path);
+            geo.normalize();
+
+            // 清空控制点
+            this.fromDivergenceControlPoint1 = null;
+            this.fromDivergenceControlPoint2 = null;
+            this.toDivergenceControlPoint1 = null;
+            this.toDivergenceControlPoint2 = null;
+        }
+        else {
+            var path = null;
+
+            // 创建链接线(折线)
+            for (var i = fromIndex; i <= toIndex; i++) {
+                var p = this.getPoint(i);
+
+                if (path === null) {
+                    path = new go.PathFigure(p.x, p.y, false);
+                }
+                else {
+                    path.add(new go.PathSegment(go.PathSegment.Line, p.x, p.y));
+                }
+            }
+
+            // 绘制链接的几何图形
+            geo.add(path);
+            geo.normalize();
+        }
+
+        return geo;
     }
 };
 
